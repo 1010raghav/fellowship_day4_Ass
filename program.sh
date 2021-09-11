efd67e0e73ea2223efadcc96a5f90b2bb5f692df
Raghav <raghavwandre1234@gmail.com>
2021-09-11 08:28:19 +0530

    program.cs

---
+++ program.sh
@@ -0,0 +1,19 @@
+using System;
+
+namespace day3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1_EmployeeAttendance.GetAttendance();
+            // UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.CalculateWagesFor20DaysInMonth();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            //UC7_ComputeEmpWageUsingClassMethod
+            UC8_ComputeEmpWageForMultipleCompanies
+        }
+    }
+}
